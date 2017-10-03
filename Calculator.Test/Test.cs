023469e498ed6f0dcee0d3166833fdf8e0a0639a
trungngotdt <trungngotdt@outlook.com.vn>
2017-10-03 13:49:15 +0700

    version 1.0

---
+++ Calculator.Test/Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SampleAppVeyor;
+
+namespace Calculator.Test
+{
+    [TestFixture]
+    public class Test
+    {
+        private SampleAppVeyor.Calculator _calculator;
+
+        [SetUp]
+        public void setup()
+        {
+            _calculator = new SampleAppVeyor.Calculator();
+        }
+        [Test]
+        public void OnePlusOne()
+        {
+            Assert.AreEqual(2, _calculator.Add(1, 1));
+
+        }
+        [Test]
+        public void ThreeSubOne()
+        {
+            Assert.AreEqual(2, _calculator.Subtract(3, 1));
+        }
+    }
+}
---
+++ SampleAppVeyor/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleAppVeyor
+{
+
+    public class Calculator
+    {
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
+
